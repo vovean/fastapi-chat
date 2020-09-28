d81550c7e29861e7260dc567b0c7a0c986c8a542
Vislovich Vladimir <vislovichv@mail.ru>
2020-09-28 20:53:59 +0300

    Allow all methods in CORS

--- views/main.py
+++ views/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
-    allow_methods=("GET", "POST"),
+    allow_methods=("*",),
     allow_headers=("*",),
     allow_credentials=True
 )
